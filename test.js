dd9a6b8dd353819428a6d15856a9f1c7cf282602
pemrouz <p3pedram@hotmail.com>
2015-07-02 03:45:37 +0100

    more browser friendly date creation

--- test.js
+++ test.js
@@ -5,19 +5,19 @@ var expect = require('chai').expect
 describe('mo', function() {
   
   it('should create date', function() {
-    expect(mo('1-30-1970').toString()).to.equal('Fri Jan 30 1970 00:00:00 GMT+0000')
+    expect(mo('30 JAN 1970').toString()).to.equal('Fri Jan 30 1970 00:00:00 GMT+0000')
   })
 
   it('should format to specific format', function() {
-    expect(mo.format('Do MMM YY')('1-30-1970')).to.equal('30th Jan 70')
+    expect(mo.format('Do MMM YY')('30 JAN 1970')).to.equal('30th Jan 70')
   })
 
   it('should format to iso by default', function() {
-    expect(mo.format()('1-30-1970')).to.equal('1970-01-30T00:00:00+00:00')
+    expect(mo.format()('30 JAN 1970')).to.equal('1970-01-30T00:00:00+00:00')
   })
   
   it('should format to simple iso by default', function() {
-    expect(mo.iso('1-30-1970')).to.equal('1970-01-30')
+    expect(mo.iso('30 JAN 1970')).to.equal('1970-01-30')
   })
 
 })
